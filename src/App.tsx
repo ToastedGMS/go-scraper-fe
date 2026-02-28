7f4f0bc5cc88dc5faeaa05a1a8386394a887ff32
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-02-28 17:12:03 -0300

    feat: implement data fetching and loading state for stories in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react';
+import Story from './features/article/Story';
+import type StoryData from './features/article/types/StoryData';
+
 function App() {
+	const [data, setData] = useState<StoryData[] | null>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(
+				'http://localhost:8080/search?query=epstein',
+			);
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			const result = await response.json();
+			setData(result);
+		};
+
+		fetchData();
+	}, []);
 	return (
 		<>
-			<span>Hello World!</span>
+			{data === null ? (
+				<p>Loading...</p>
+			) : (
+				<div>
+					{data.map((story, index) => (
+						<Story
+							key={index}
+							headline={story.headline}
+							article={story.article}
+						/>
+					))}
+				</div>
+			)}
 		</>
 	);
 }
